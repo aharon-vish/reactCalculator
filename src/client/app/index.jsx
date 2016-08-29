aca77d301b02e46c5b4ef8e4f9a34bf693fb66af
aharon-vish <aharon.vish@gmail.com>
2016-08-29 20:48:19 +0300

    Dsfs

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import {render} from 'react-dom';
-import AwesomeComponent from './AwesomeComponent.jsx';
 import InputDigit from './InputDigit.jsx';
 import KeyBoard from './KeyBoard.jsx';
 class App extends React.Component {
